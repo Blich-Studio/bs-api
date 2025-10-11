2e966bb18ebb42f2189e57cf1f5ec622a398afc6
Filip Cerny <f33koslav@gmail.com>
2025-10-11 11:24:47 +0200

    feat(BLICH-36): project structure, tests, structure, jest setup, updated package.json

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts']
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const roots = ['<rootDir>/src'];
+export const testMatch = ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'];
+export const collectCoverageFrom = [
+  'src/**/*.ts',
+  '!src/**/*.d.ts',
+  '!src/app.ts',
+  '!src/server.ts'
+];
+export const coverageThreshold = {
+  global: {
+    branches: 80,
+    functions: 80,
+    lines: 80,
+    statements: 80
+  }
 };
