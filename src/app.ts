c471f2c99006c8896fceb0c0ce5f79cb35cfbfe1
Filip Cerny <f33koslav@gmail.com>
2025-10-12 10:48:34 +0200

    feat(BLICH-36): logger setup

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
+import helmet from 'helmet'
 
-import healthRouter from './routes/health'
+import morganMiddleware from './middleware/morgan'
+
+//FIXME
+//import healthRouter from './routes/health'
 
 const app = express()
 const swaggerDocument = YAML.load('./openapi.yaml')
 
+app.use(helmet())
+app.use(morganMiddleware)
 // Serve API docs (not versioned — keep this global)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+//FIXME delete this before deploy to prod
+app.get('/logger', (_, res) => {
+  res.send('Hello world')
+})
+
 // ✅ Base API version path
-app.use('/v1/health', healthRouter)
+//app.use('/v1/health', healthRouter)
 
 export default app
